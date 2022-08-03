49c2dbe91693fcc5fedcf459d25db0a776d6e254
halitov-r-k <halitov.r.k@gmail.com>
2022-08-03 16:53:38 +0500

    2022-08-03T17:00

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,58 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
+    public static Connection getConnection() {
+        Properties properties = new Properties();
+        InputStream inputStream;
+        try {
+            inputStream = Files.newInputStream(Paths.get("database.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String url = properties.getProperty("url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String driver = properties.getProperty("driver");
+
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+            System.out.println("Driver OK");
+        } catch (Exception e) {
+            System.out.println("Driver No");
+        }
+
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Util: Close connection");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
---
+++ src/test/java/TestDB.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
+
+public class TestDB {
+    public static void main(String[] args) {
+        System.out.println("test db");
+
+        Connection connection = getConnection();
+    }
+}
