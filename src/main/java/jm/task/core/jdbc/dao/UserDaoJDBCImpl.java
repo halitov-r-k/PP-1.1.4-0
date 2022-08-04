cd8c7f3be23804ff55a1ab959e1af497bfbd6c65
halitov-r-k <halitov.r.k@gmail.com>
2022-08-04 16:58:11 +0500

    2022-08-04T17:00 Тест создание таблицы

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,40 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl implements UserDao {
+    static Logger LOGGER = Logger.getLogger(UserDaoJDBCImpl.class.getName());
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        LOGGER.log(Level.INFO,"Create Table Users ");
     }
 
     public void dropUsersTable() {
-
+        LOGGER.log(Level.INFO,"Drop Table Users ");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        LOGGER.log(Level.INFO,"Save User ");
     }
 
     public void removeUserById(long id) {
-
+        LOGGER.log(Level.INFO,"Remove User By Id ");
     }
 
     public List<User> getAllUsers() {
+        LOGGER.log(Level.INFO,"Get All Users ");
         return null;
     }
 
     public void cleanUsersTable() {
-
+        LOGGER.log(Level.INFO,"Clean Table Users ");
     }
 }
